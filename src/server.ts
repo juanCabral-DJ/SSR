227267a63d345937dc630788ce755ecdaf02a68f
juanCabral-DJ <juandavidcabral71@gmail.com>
2026-05-11 08:19:35 -0700

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,28 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context.mjs';
 
-const angularAppEngine = new AngularAppEngine();
+const angularAppEngine = new AngularAppEngine({
+  allowedHosts: [
+    'ssr-pokemonssr.netlify.app',
+    '*.netlify.app',
+  ],
+  trustProxyHeaders: [
+    'x-forwarded-for',
+    'x-forwarded-host',
+    'x-forwarded-proto',
+  ],
+});
 
 export async function netlifyAppEngineHandler(
   request: Request,
 ): Promise<Response> {
-
   const context = getContext();
 
-  const result = await angularAppEngine.handle(
-    request,
-    context
-  );
+  const result = await angularAppEngine.handle(request, context);
 
   return result || new Response('Not found', {
     status: 404,
   });
 }
 
-/**
- * Angular CLI request handler
- */
-export const reqHandler =
-  createRequestHandler(netlifyAppEngineHandler);
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
